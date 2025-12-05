981426a1d0ab0bc387159a708c2ca952f7b78831
komiljon <berdiboevkomiljon@gmail.com>
2025-12-06 02:43:35 +0500

    changes for deploy

--- src/main/java/org/example/bot/DevBot.java
+++ src/main/java/org/example/bot/DevBot.java
@@ -43,13 +43,24 @@ public class DevBot extends TelegramLongPollingBot {
         }
     }
 
-    @Override
+    /*@Override
     public String getBotUsername() {
         return dotenv.get("BOT_USERNAME");
+    }*/
+
+    @Override
+    public String getBotUsername() {
+        return System.getenv("BOT_USERNAME");
     }
+
+
+    /*@Override
+    public String getBotToken() {
+        return dotenv.get("BOT_TOKEN");
+    }*/
 
     @Override
     public String getBotToken() {
-        return dotenv.get("BOT_TOKEN");
+        return System.getenv("BOT_TOKEN");
     }
 }
